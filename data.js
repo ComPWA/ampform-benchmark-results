9099ebd0c63e0da3ad603ce93250a91ac557d685
github-action-benchmark <github@users.noreply.github.com>
2024-05-22 12:50:26 +0000

    add AmpForm benchmark results (pytest) benchmark result for effb0ded2f74b7fdebb077f272dfbce0f8addae6

---
+++ data.js
@@ -0,0 +1,39 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1716382226121,
+  "repoUrl": "https://github.com/ComPWA/ampform",
+  "entries": {
+    "AmpForm benchmark results": [
+      {
+        "commit": {
+          "author": {
+            "email": "29308176+redeboer@users.noreply.github.com",
+            "name": "Remco de Boer",
+            "username": "redeboer"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "effb0ded2f74b7fdebb077f272dfbce0f8addae6",
+          "message": "DX: implement benchmark monitoring (#427)",
+          "timestamp": "2024-05-22T20:44:32+08:00",
+          "tree_id": "bb6cee38b71ef1ae116d90b0732b36212223a02f",
+          "url": "https://github.com/ComPWA/ampform/commit/effb0ded2f74b7fdebb077f272dfbce0f8addae6"
+        },
+        "date": 1716382225705,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "benchmarks/doit_speed.py::test_doit_speed",
+            "value": 0.18574261911364154,
+            "unit": "iter/sec",
+            "range": "stddev: 0",
+            "extra": "mean: 5.383794009000042 sec\nrounds: 1"
+          }
+        ]
+      }
+    ]
+  }
+}
